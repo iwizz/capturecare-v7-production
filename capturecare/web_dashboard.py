bfe3bc7ca30401288152a01a13360417a10f8958
Tim <tim@wizz.com.au>
2025-12-07 15:09:26 +1100

    Fix login_manager endpoint for blueprint compatibility
    - Changed login_view from 'login' to 'auth.login'
    - Fixes 500 error when unauthorized users access protected routes
    - Required after moving login to auth blueprint

--- capturecare/web_dashboard.py
+++ capturecare/web_dashboard.py
@@ -192,7 +192,7 @@ def index():
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'auth.login'
 login_manager.login_message = 'Please log in to access this page.'
 login_manager.login_message_category = 'info'
 
